6d6ced2a929d46bcbee3efd58e3d1a9ebf4b6b42
Paul Vidor <paul.vidor.travail@gmail.com>
2020-12-11 09:07:10 +0100

    MrJackPocket2020Q1ISEP
    Il s'agit d'un projet de premier semestre en JAVA visant à reconstituer un jeu de société.

--- .idea/README.md
+++
@@ -1,2 +0,0 @@
-# MrJackPocket2020A1ISEP
-Projet ISEP 2020 A1 MrJackPocket
---
+++ src/com/vidorpaul/Main.java
@@ -0,0 +1,11 @@
+package com.vidorpaul;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Taille du rectangle est : ");
+    }
+}
